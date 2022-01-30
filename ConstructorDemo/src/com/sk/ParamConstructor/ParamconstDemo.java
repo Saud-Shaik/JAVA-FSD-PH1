ff9cfcf666c2eeef62e8753c156444651b9336a8
sk saud <soudshaik18@gmail.com>
2022-01-30 22:26:05 +0530

    first commit

---
+++ ConstructorDemo/src/com/sk/ParamConstructor/ParamconstDemo.java
@@ -0,0 +1,12 @@
+package com.sk.ParamConstructor;
+
+public class ParamconstDemo {
+	public static void main(String[] args) {
+		Student std1=new Student(2,"Alex");
+		Student std2=new Student(10,"Annie");
+		std1.display();
+		std2.display();
+
+	}
+
+}
---
+++ ConstructorDemo/src/com/sk/ParamConstructor/Student.java
@@ -0,0 +1,21 @@
+package com.sk.ParamConstructor;
+
+public class Student {
+	
+		int id;
+		String name;
+
+		Student(int i,String n)
+		{
+		id=i;
+		name=n;
+		}
+
+		void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
+	
+
+
