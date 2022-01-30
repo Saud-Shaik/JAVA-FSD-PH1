0781cc82aa8515f9b4555ef4ce710739820c0d47
sk saud <soudshaik18@gmail.com>
2022-01-30 22:11:05 +0530

    First commit

---
+++ MethodOverLoading/src/com/sk/OverLoading/OverLoad.java
@@ -0,0 +1,25 @@
+package com.sk.OverLoading;
+
+public class OverLoad {
+	
+		
+		public void area(int b,int h)
+		    {
+		         System.out.println("Area of Triangle : "+(0.5*b*h));
+		    }
+		    public void area(int r) 
+		    {
+		         System.out.println("Area of Circle : "+(3.14*r*r));
+		    }
+
+		    public static void main(String args[])
+		   {
+
+		OverLoad ob=new OverLoad();
+		       ob.area(10,12);
+		       ob.area(5);  
+		   }
+		}
+
+
+
